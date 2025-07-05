753cddb52c961b7eac128417c969f42d7e70c457
Ukhasyah Ganteng <ukasyaaah@gmail.com>
2025-07-05 23:05:19 +0700

    Str Funct

---
+++ NewStrFun.php
@@ -0,0 +1,9 @@
+<?php
+// Apakah Ukhasyah Zufar mengandung Zufar
+var_dump(str_contains('Ukhasyah Zufar', 'Zufar'));
+
+// Apakah Hani Fauzan diakhiri Fauzan
+var_dump(str_ends_with("Hani Fauzan", "Fauzan"));
+
+// Apakah Hani Fauzan diawali Fauzan
+var_dump(str_starts_with("Hani Fauzan", "Fauzan"));
